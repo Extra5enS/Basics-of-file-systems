1ffa8392175e38f2c8c11b8501691d43a73fb535
Extra5enS <molotkov.md@phystech.edu>
2020-10-28 17:34:49 +0300

    add scanf parting

--- my_lsof/solution.c
+++ my_lsof/solution.c
@@ -29,82 +29,44 @@ typedef struct {
 void pid_stat_read(int pid) {
     char pathname[MAX_MEM_SIZE];
     memset(pathname, '\0', MAX_MEM_SIZE);
-    strcat(pathname, "/proc/");
-    
-    char pid_in_str[MAX_PIDSTR_SIZE];
-    memset(pid_in_str, '\0', MAX_PIDSTR_SIZE);
-    sprintf(pid_in_str, "%d", pid);
-    strcat(pathname, pid_in_str);
+    snprintf(pathname, sizeof(pathname), "/proc/%d/stat", pid);
 
-    struct stat mystat;
-    if(stat(pathname, &mystat) == -1) {
-			perror("Stat error");
-			exit(-1);
-    }
-     
-    char stat[MAX_MEM_SIZE];
-    memset(stat, '\0', MAX_MEM_SIZE);
-    strcat(stat, pathname);
-    strcat(stat, "/stat");
 
     // Now we have stat = "/proc/[pid]/stat";
     // So we can read "stat" to print information;
     char strstat[MAX_MEM_SIZE];
+    struct stat mystat;
     memset(strstat, '\0', MAX_MEM_SIZE);
     int fd = 0; 
-    if((fd = open(stat, O_RDONLY)) == -1) {
+    if((fd = open(pathname, O_RDONLY)) == -1) {
         perror("Open error");
         exit(-1); 
+    }
+    if(fstat(fd, &mystat) == -1) {
+			perror("Stat error");
+			exit(-1);
     }
     if(read(fd, strstat, MAX_MEM_SIZE) == -1) {
         perror("Read error");
         exit(-1);
     }
     close(fd);
-
-    // Now we will use sscanf to select information
     pid_stat_t pid_stat;
-    char* left_pointer = strstat;
-    char* right_pointer = strstat;
-    struct passwd* user = getpwuid(mystat.st_uid);
+    
     char comm[MAX_MEM_SIZE];
-    for(int i = 1; i < 52; ++i) {
-        right_pointer = strstr(left_pointer, " ");
-        
-        if(right_pointer != NULL && i != 2) {
-            *right_pointer = '\0';
-        } else if(i == 2) {
-            while(*(right_pointer + 2) != ' ') {
-                *right_pointer = '_';
-                right_pointer = strstr(right_pointer, " "); 
-            }
-        }
-
-        switch(i) {
-            case 1: // pid
-                sscanf(left_pointer, "%d", &pid_stat.pid);
-                break;
-            case 2: // comm
-                sscanf(left_pointer, "%s", comm);
-                comm[strlen(comm) - 1] = '\0';
-                pid_stat.comm = comm + 1;
-                break;
-        }
-        left_pointer = right_pointer + 1;
-        if(right_pointer == NULL) {
-            break;
-        }
-
-    }
+    sscanf(strstat, "%d %[A-Z, a-z, ,(,.])", &pid_stat.pid, comm);
+    pid_stat.comm = comm + 1;
+    
+    // Now we will use sscanf to select information
+    struct passwd* user = getpwuid(mystat.st_uid);
+    
+    
     //Now we must read /proc/[pid]/fd to find fd
-     
-    char fd_dir[MAX_MEM_SIZE];
-    memset(fd_dir, '\0', MAX_MEM_SIZE);
-    strcat(fd_dir, pathname);
-    strcat(fd_dir, "/fd/");
+    memset(pathname, '\0', MAX_MEM_SIZE);
+    snprintf(pathname, sizeof(pathname), "/proc/%d/fd/", pid);
     
     DIR* dir;
-    if((dir = opendir(fd_dir)) == NULL) {
+    if((dir = opendir(pathname)) == NULL) {
 		perror("Open dir error");
 		exit(-1);
 	}
@@ -118,7 +80,7 @@ void pid_stat_read(int pid) {
         char fd_info[MAX_MEM_SIZE];
         memset(fd_info, '\0', MAX_MEM_SIZE);
         
-        strcat(fd_info, fd_dir);
+        strcat(fd_info, pathname);
         strcat(fd_info, dirbuf -> d_name);
    
         struct stat linkstat;
