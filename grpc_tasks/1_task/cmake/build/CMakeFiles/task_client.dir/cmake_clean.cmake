2f70e98b054857f0c0084b3402570498f70667fd
Extra5enS <molotkov.md@phystech.edu>
2020-12-16 14:26:44 +0300

    add done 1 task and start part of 2 task

---
+++ grpc_tasks/1_task/cmake/build/CMakeFiles/task_client.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task_client.dir/task.grpc.pb.cc.o"
+  "CMakeFiles/task_client.dir/task.pb.cc.o"
+  "CMakeFiles/task_client.dir/task_client.cc.o"
+  "task.grpc.pb.cc"
+  "task.grpc.pb.h"
+  "task.pb.cc"
+  "task.pb.h"
+  "task_client"
+  "task_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ grpc_tasks/2_task/cmake/build/CMakeFiles/task_server.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task_server.dir/task.grpc.pb.cc.o"
+  "CMakeFiles/task_server.dir/task.pb.cc.o"
+  "CMakeFiles/task_server.dir/task_server.cc.o"
+  "task.grpc.pb.cc"
+  "task.grpc.pb.h"
+  "task.pb.cc"
+  "task.pb.h"
+  "task_server"
+  "task_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
